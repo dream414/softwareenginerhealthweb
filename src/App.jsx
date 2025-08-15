2e9984c27ed39d8c65864b11bb9a4aff9119a86b
dream414 <developerfrontend00@gmail.com>
2025-08-15 01:41:20 -0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import Navba from "./Navba";
 import ResponsiveHomeHero from './ResponsiveHomeHero.jsx'
-import FourBoxes from './FourBoxes.JSX'
+import FourBoxes from './FourBoxes.jsx'
 import AboutSection from './about.jsx'
 import AboutWithImage from './imagetext.jsx'
 import RankingBars from './rankinglist.jsx'
 import SimpleForm from "./SimpleForm.jsx";
 import Footer from "./Foote.jsx";
 import SixBoxesSlider from "./SixBoxesSlider.jsx";
-import Navbar from "./navbar.jsx";
+
 
 
 export default function App() {
   return (<>
- <Navbar/>
     <Navba/>
     <ResponsiveHomeHero/>
     <FourBoxes/>
